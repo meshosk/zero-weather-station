65e8a3029df64ff62ddc5845b4a9a8260311e697
Matej Meško <meshosk@gmail.com>
2025-04-13 17:57:00 +0200

    added image generation script

---
+++ image-generator/image-generator.py
@@ -0,0 +1,17 @@
+# this file should generate 4bit grayscale image that can be used directly into wireshare eing display
+# image size for waveshare 7.8 HD epaper is 1872×1404 
+
+from PIL import Image
+import numpy as np
+
+
+# 1. Vytvorenie nového 8-bit grayscale obrázka (napr. gradient)
+width, height = 256, 100
+gradient = np.tile(np.arange(width, dtype=np.uint8), (height, 1))
+img = Image.fromarray(gradient, mode='L')  # 8-bit grayscale
+
+
+
+
+
+img.
